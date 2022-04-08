a6b583b773d8a75029fe8e773694eb85e00a9811
rafael.oliveira <rafaeloliveira@Rafaels-MacBook-Pro.local>
2022-04-08 12:44:10 +0100

    basic fronted setup

---
+++ client/layouts/default.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar
+      app
+      color="white"
+      flat
+    >
+
+      <v-tabs
+        centered
+        class="ml-n9"
+        color="grey darken-1"
+      >
+        <v-tab
+          v-for="link in links"
+          :key="link.name"
+          :to="link.link"
+        >
+          {{ link.name }}
+        </v-tab>
+      </v-tabs>
+    </v-app-bar>
+
+    <v-main class="grey lighten-3">
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data () {
+    return {
+      links:[
+        {name: 'Index', link: '/'}
+      ]
+    };
+  }
+}
+</script>
---
+++ client/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-sheet min-height='85vh' rounded='lg'>
+  </v-sheet>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
